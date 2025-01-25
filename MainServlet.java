ef026d4fa04b391a3465b2115b5a697e17275e73
assilagu <151388080+assilagu@users.noreply.github.com>
2025-01-25 18:33:43 +0100

    Create MainServlet.java

---
+++ MainServlet.java
@@ -0,0 +1,17 @@
+package com.example.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/home")
+public class MainServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Bienvenue sur la Servlet MVC !</h1>");
+    }
+}
